ec4bdb15b065a88a03d0750591284566a504c286
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-05-26 20:19:00 +0300

    summary

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!')
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title)
+    })
+
+const onCurrentTime = throttle(data =>
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds)), 1000);
+    
+    const currentTime = localStorage.getItem("videoplayer-current-time");
+
+    player.setCurrentTime(currentTime).then(function (seconds) {
+    }).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+    });
+
+    player.on('timeupdate', onCurrentTime);
